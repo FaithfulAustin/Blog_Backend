6c614d35e0c5982856607dd346326800eef2ed04
JEDIBASIL <jedidiahbasil@gmail.com>
2024-05-27 12:58:22 +0100

    resolving issue

--- src/api/route/UserRoute.ts
+++ src/api/route/UserRoute.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import userController from "../Controllers/User/userController";
-import JWTVaildator from "../middleware/JWTVaildator"
+import JWTValidator from "../middleware/JWTVaildator"
 
 
 const UserController: userController = new userController()
 const UserRouter = Router();
 
-UserRouter.get('/userProfile',JWTVaildator.isLoggedIn,UserController.getUserProfile)
-UserRouter.post('/addCategoriesToUser',JWTVaildator.isLoggedIn,UserController.addCategoriesToUser)
-UserRouter.post('/followAUser/:id',JWTVaildator.isLoggedIn,UserController.followAUser)
-UserRouter.post('/UpdateUserProfile',JWTVaildator.isLoggedIn,UserController.UpdateUserProfile)
-UserRouter.get('/viewUser/:id',JWTVaildator.isLoggedIn,UserController.viewAUserDetails)
-UserRouter.get('/allUsers',JWTVaildator.isLoggedIn,UserController.getListOfUsers)
+UserRouter.get('/userProfile', JWTValidator.isLoggedIn, UserController.getUserProfile)
+UserRouter.post('/addCategoriesToUser', JWTValidator.isLoggedIn, UserController.addCategoriesToUser)
+UserRouter.post('/followAUser/:id', JWTValidator.isLoggedIn, UserController.followAUser)
+UserRouter.post('/UpdateUserProfile', JWTValidator.isLoggedIn, UserController.UpdateUserProfile)
+UserRouter.get('/viewUser/:id', JWTValidator.isLoggedIn, UserController.viewAUserDetails)
+UserRouter.get('/allUsers', JWTValidator.isLoggedIn, UserController.getListOfUsers)
 
 
-export default  UserRouter
+export default UserRouter
