eb2f22c139385f3ae15ea8e413a1f84bad901661
JEDIBASIL <jedidiahbasil@gmail.com>
2024-06-19 21:11:28 +0100

    fixed auth route error

--- src/route/AuthRoute.ts
+++ src/route/AuthRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import AuthController from "../Controllers/auth/authController";
+import AuthController from "../controllers/auth/authController";
 import dtoValidationMiddleware from "../middleware/dto.validator.middleware";
 import { EmailDto } from "../dto/SiginUpDto";
 
