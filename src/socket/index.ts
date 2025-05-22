f6f36f2768674ec10e0c8e4a194460e5c92f4841
JEDIBASIL <jedidiahbasil@gmail.com>
2025-05-22 10:14:23 +0100

    worked on saving and updating post

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -4,6 +4,7 @@ import Jwt from "../utils/jwt";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
 import SocketUserGuard from "../guard/socket-user.guard";
 import { ExtendedError } from "socket.io/dist/namespace";
+import PostService from "../services/post.service";
 
 
 interface CustomSocket extends Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap> {
@@ -35,18 +36,20 @@ export default async function createSocket(server: Server<typeof IncomingMessage
         next();
     });
 
-    io.on('connection', (socket) => {
+    io.on('connection', async (socket) => {
         const customSocket = socket as CustomSocket;
         console.log('User connected:', customSocket.user);
 
-        socket.on('disconnect', () => {
+        socket.on('disconnect', async () => {
             console.log('User disconnected');
         });
         socket.on('post', async (postId: string, content: string) => {
-            setTimeout(() => {
-                socket.emit('post', { message: 'Note saved successfully', content: "hi" });
-            }, 3000);
-
+            console.log(postId)
+            console.log(content)
+            const postService = new PostService()
+            const isSaved = await postService.createPost(postId, customSocket.user, content)
+            console.log("isSaved", isSaved)
+            if (isSaved) socket.emit('post', { postId: isSaved.postId, message: 'Note saved successfully', content: "hi" });
         });
     });
 }
